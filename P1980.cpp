23fec413974bce654827c758b3efc8486c2796c7
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2019-07-28 16:37:11 +0800

    Create P1980.cpp

---
+++ P1980.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long last,i,j,counter,target;
+    cin>>last>>target;
+    for(i=1;i<=last;i++){
+        j=i;
+        for(;j>0;j/=10){
+            if((j%10)==target){
+                counter++;
+            }
+        }
+    }
+    cout<<counter;
+    return 0;
+}
