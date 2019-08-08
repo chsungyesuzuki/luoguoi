9034729b9be55ff2795bf9e81ba024632839c3c8
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2019-08-08 13:58:38 +0800

    Update P1591.cpp

--- P1591.cpp
+++ P1591.cpp
@@ -1,10 +1,9 @@
-//This program has a serious problem. #2
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
 struct High{
 	int value[2600],length;
-	High(int rvalue=1){
+	High(int rvalue=0){
 		memset(value,0,sizeof(value));
 		length=1;
 		for(;rvalue>0;length++){
@@ -13,72 +12,48 @@ struct High{
 		}
 		length--;
 	}
-	int &operator[](int index){
+	int get(int index){
 		return value[index];
 	}
 	void flatten(int newLength){
 		length=newLength;
 		for(int i=1;i<=length;i++){
-			value[i+1]=value[i+1]+value[i]/10;
-			value[i]=value[i]%10;
+			value[i+1]+=value[i]/10;
+			value[i]%=10;
 		}
 		for(;value[length]==0;){
 			length--;
 		}
 	}
-	High operator*(int factorInteger){
+	High star(int factorInteger){
 		High answer;
 		for(int i=1;i<=length;i++){
 			answer.value[i]=value[i]*factorInteger;
 		}
-		flatten(length+12);
-	}
-	void printToStdout(){
-		for(int i=length;i>=1;i--){
-			printf("%d",value[i]);
-		}
+		answer.flatten(length+20);
+		return answer;
 	}
 };
-//void printHighToStdoutAndThenNewALineToPutSomethingElseMoreEasilySoYouDontHaveToWriteTwoLinesOfCodesEveryTime(High high){
-//	high.printToStdout();
-//	putchar('\n');
-//}
 High getStairStar(int n){
-	High answer;
-//	puts("awa");
-//	printHighToStdoutAndThenNewALineToPutSomethingElseMoreEasilySoYouDontHaveToWriteTwoLinesOfCodesEveryTime(answer);
+	High answer (1);
 	for(int i=2;i<=n;i++){
-//		puts("qwq");
-		answer=answer*i;
-//		printHighToStdoutAndThenNewALineToPutSomethingElseMoreEasilySoYouDontHaveToWriteTwoLinesOfCodesEveryTime(answer);
+		answer=answer.star(i);
 	}
 	return answer;
 }
-void test(){
-	High h;
-	h[1]=9;
-	h[2]=8;
-	h[3]=17;
-	h.flatten(4);
-	h.printToStdout();
-}
 int main(){
-	High high1(20);
-	High high2=high1*20;
-//	printf("%d \n",high2[1]);
-	test();
 	int t=0;
 	scanf("%d",&t);
 	for(int i=0;i!=t;i++){
 		int n=0,a=0,counter=0;
 		scanf("%d%d",&n,&a);
 		High stairStar=getStairStar(n);
-		for(int j=0;j!=stairStar.length;j++){
-			if(stairStar[j]==a){
+		for(int j=1;j<=stairStar.length;j++){
+			if(stairStar.get(j)==a){
 				counter++;
 			}
 		}
-//		counter++;
 		printf("%d\n",counter);
 	}
+	return 0;
 }
