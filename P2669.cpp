7409992785d9ee9481442721e99f67aa0aa755ee
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2019-08-10 11:39:47 +0800

    Update P2669.cpp

--- P2669.cpp
+++ P2669.cpp
@@ -16,3 +16,23 @@ int main(){
 	printf("%lld\n",answer);
 	return 0;
 }
+//O(n+1)
+#include <cstdio>
+int main(){
+	int k,n;
+	long long answer=0,last;
+	scanf("%d",&k);
+	for(n=1;true;n++){
+		if((1+n)*n/2>k){
+			n--;
+			last=(k-(1+n)*n/2)*(n+1);
+			break;
+		}
+	}
+	for(int i=n;i>=1;i--){
+		answer+=(n*2-i+1)*i/2;
+	}
+	answer+=last;
+	printf("%lld\n",answer);
+	return 0;
+}
